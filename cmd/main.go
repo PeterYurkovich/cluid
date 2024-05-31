9559971384bb59247fd018f2ad1195a0b22946d0
PeterYurkovich <pyurkovi@redhat.com>
2024-05-31 16:00:20 -0400

    Add on initial UI

--- cmd/main.go
+++ cmd/main.go
@@ -2,76 +2,104 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/peteryurkovich/cluid/pkg/apply"
 )
 
-const url = "https://charm.sh/"
-
-type model struct {
-	status int
-	err    error
+func getPath(templateName string) string {
+	switch templateName {
+	case "Deployment":
+		return "/templates/hack/openshift/config/deployment"
+	case "Operators":
+		return "/templates/hack/openshift/config/operators"
+	case "Resources":
+		return "/templates/hack/openshift/config/resources"
+	default:
+		return ""
+	}
 }
 
-func checkServer() tea.Msg {
-
-	err := apply.Apply("templates/hack/openshift/config/deployment")
-	if err != nil {
-		return errMsg{err}
-	}
-
-	return statusMsg("200")
-}
-
-type statusMsg string
-
-type errMsg struct{ err error }
-
-// For messages that contain errors it's often handy to also implement the
-// error interface on the message.
-func (e errMsg) Error() string { return e.err.Error() }
-
-func (m model) Init() tea.Cmd {
-	return checkServer
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case statusMsg:
-		m.status = 200
-		return m, tea.Quit
-
-	case errMsg:
-		m.err = msg
-		return m, tea.Quit
-
-	case tea.KeyMsg:
-		if msg.Type == tea.KeyCtrlC {
-			return m, tea.Quit
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) View() string {
-	if m.err != nil {
-		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
-	}
-
-	s := fmt.Sprintf("Checking %s ... ", url)
-	if m.status > 0 {
-		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
-	}
-	return "\n" + s + "\n\n"
+type State struct {
+	Templates []string
 }
 
 func main() {
-	if _, err := tea.NewProgram(model{}).Run(); err != nil {
-		fmt.Printf("Uh oh, there was an error: %v\n", err)
+	var state = State{Templates: []string{}}
+
+	accessible, _ := strconv.ParseBool(os.Getenv("ACCESSIBLE"))
+
+	form := huh.NewForm(
+		huh.NewGroup(huh.NewNote().
+			Title("Cluid").
+			Description("Welcome to _Cluid_.\n\nWhat would you like to deploy today?\n\n").
+			Next(true).
+			NextLabel("Let's go!"),
+		),
+
+		huh.NewGroup(
+			huh.NewMultiSelect[string]().
+				Title("Deployments").
+				Description("Select your desired deployments.").
+				Options(
+					huh.NewOption("Deployment", "Deployment").Selected(true),
+					huh.NewOption("Operators", "Operators"),
+					huh.NewOption("Resources", "Resources"),
+				).
+				Validate(func(templates []string) error {
+					if len(templates) <= 0 {
+						return fmt.Errorf("at least one topping is required")
+					}
+					return nil
+				}).
+				Value(&state.Templates).
+				Filterable(true).
+				Limit(4),
+		),
+	).WithAccessible(accessible)
+
+	err := form.Run()
+
+	if err != nil {
+		fmt.Println("Uh oh:", err)
 		os.Exit(1)
+	}
+
+	action := func() {
+		for _, template := range state.Templates {
+			apply.Apply(getPath(template))
+		}
+	}
+	spinner.New().Title("Applying Deployments...").Action(action).Run()
+
+	{
+		formatDeployments := func(s string) string {
+			return lipgloss.NewStyle().Foreground(lipgloss.Color("212")).Render(s)
+		}
+		readableDeployments := []string{}
+		for _, t := range state.Templates {
+			readableDeployments = append(readableDeployments, formatDeployments(t))
+		}
+		var sb strings.Builder
+
+		fmt.Fprintf(&sb,
+			"%s\n\n%s",
+			lipgloss.NewStyle().Bold(true).Render("Deployment Results"),
+			strings.Join(readableDeployments, "\n"),
+		)
+
+		fmt.Println(
+			lipgloss.NewStyle().
+				Width(40).
+				BorderStyle(lipgloss.RoundedBorder()).
+				BorderForeground(lipgloss.Color("63")).
+				Padding(1, 2).
+				Render(sb.String()),
+		)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,22 @@ go 1.22.0
 
 require k8s.io/kubectl v0.30.1
 
-require github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.18.0 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
+	github.com/charmbracelet/x/ansi v0.1.1 // indirect
+	github.com/charmbracelet/x/exp/strings v0.0.0-20240524151031-ff83003bf67a // indirect
+	github.com/charmbracelet/x/input v0.1.1 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.2 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+)
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
@@ -12,7 +27,10 @@ require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/chai2010/gettext-go v1.0.2 // indirect
-	github.com/charmbracelet/bubbletea v0.26.2 // indirect
+	github.com/charmbracelet/bubbletea v0.26.3 // indirect
+	github.com/charmbracelet/huh v0.4.2
+	github.com/charmbracelet/huh/spinner v0.0.0-20240529143420-2ae64435bd5d
+	github.com/charmbracelet/lipgloss v0.11.0
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
@@ -46,7 +64,7 @@ require (
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
@@ -63,7 +81,7 @@ require (
 	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rivo/uniseg v0.4.6 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
@@ -75,7 +93,7 @@ require (
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/term v0.20.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
